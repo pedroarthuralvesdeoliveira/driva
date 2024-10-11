57eb7027c25f55fcebac04a5cec59ae966862901
Pedro <pedroarthuralvesdeoliveira@hotmail.com>
2024-10-10 22:36:22 -0300

    Melhor dia em vendas

--- teste_driva/app.py
+++ teste_driva/app.py
@@ -5,7 +5,7 @@ from etl import MarketData
 etl = MarketData('data/Cópia de Teste visualização de dados.xlsx')
 etl.load_sales()
 etl.load_products()
-etl.best_selling_days()
+etl.best_selling_day()
 etl.daily_sales()
 etl.sales_per_product()
 etl.best_selling_product_in_kg()
@@ -13,10 +13,14 @@ etl.best_banana_selling_day()
 
 st.title('Teste Driva - Análise de vendas na Feira')
 
-st.header('Evolução do faturamento ao longo do tempo')
+st.header('1 - Evolução do faturamento ao longo do tempo')
 daily_sales = etl.con.execute("SELECT * FROM daily_sales").fetchdf()
 fig = px.line(daily_sales, x='DATA', y='TOTAL_VENDA', title='Faturamento diário')
 st.plotly_chart(fig)
+
+st.header('2 - Melhor dia em vendas')
+best_day, best_revenue = etl.best_selling_day()
+st.write(f'O melhor dia em vendas foi {best_day} com um faturamento de R${best_revenue:.2f}')
 
 if __name__ == '__main__':
     pass
--- teste_driva/etl.py
+++ teste_driva/etl.py
@@ -12,34 +12,25 @@ class MarketData:
         df['DATA'] = pd.to_datetime(df['DATA'], format='%d-%m-%y')
         df['VALOR_VENDA'] = pd.to_numeric(df['VALOR_VENDA']).round(2)
         self.con.execute("CREATE TABLE sales AS SELECT * FROM df")
-        # result = self.con.execute("SELECT * FROM sales LIMIT 10").fetchall()
-        # print(result)
 
     def load_products(self):
         df = pd.read_excel(self.data_path, sheet_name='produtos', header=0, dtype=object)
         self.con.execute("CREATE TABLE products AS SELECT * FROM df")
-        # result = self.con.execute("SELECT * FROM products LIMIT 10").fetchall()
-        # print(result)
 
-    def best_selling_days(self): 
-        result = self.con.execute("SELECT DATA, SUM(VALOR_VENDA) AS TOTAL_VENDA FROM sales GROUP BY DATA ORDER BY TOTAL_VENDA DESC LIMIT 10").fetchall()
-        print(result)
+    def best_selling_day(self): 
+        return self.con.execute("SELECT STRFTIME('%d/%m/%Y', DATA), SUM(VALOR_VENDA) AS TOTAL_VENDA FROM sales GROUP BY DATA ORDER BY TOTAL_VENDA DESC LIMIT 1").fetchone()
 
     def daily_sales(self):
-        result = self.con.execute("CREATE VIEW IF NOT EXISTS daily_sales AS SELECT DATA, SUM(VALOR_VENDA) AS TOTAL_VENDA FROM sales GROUP BY DATA ORDER BY DATA").fetchall()
-        print(result)
+        self.con.execute("CREATE VIEW IF NOT EXISTS daily_sales AS SELECT DATA, SUM(VALOR_VENDA) AS TOTAL_VENDA FROM sales GROUP BY DATA ORDER BY DATA").fetchall()
 
     def sales_per_product(self):
-        result = self.con.execute("SELECT s.ID_PRODUTO, SUM(s.VALOR_VENDA) AS TOTAL_VENDA FROM sales s INNER JOIN products p ON s.ID_PRODUTO = p.ID_PRODUTO GROUP BY s.ID_PRODUTO, p.NOME_PRODUTO ORDER BY TOTAL_VENDA DESC").fetchall()
-        print(result)
+        return self.con.execute("SELECT s.ID_PRODUTO, SUM(s.VALOR_VENDA) AS TOTAL_VENDA FROM sales s INNER JOIN products p ON s.ID_PRODUTO = p.ID_PRODUTO GROUP BY s.ID_PRODUTO, p.NOME_PRODUTO ORDER BY TOTAL_VENDA DESC").fetchall()
 
     def best_selling_product_in_kg(self):
-        result = self.con.execute("SELECT p.NOME_PRODUTO, COUNT(p.ID_PRODUTO), SUM(s.VALOR_VENDA) AS TOTAL_VENDAS, SUM((s.VALOR_VENDA / p.PREÇO_KG)) AS Quantidade_Vendida_KG FROM sales s INNER JOIN products p ON s.ID_PRODUTO = p.ID_PRODUTO GROUP BY p.NOME_PRODUTO ORDER BY Quantidade_Vendida_KG DESC LIMIT 1").fetchall()
-        print(result)
+        return self.con.execute("SELECT p.NOME_PRODUTO, COUNT(p.ID_PRODUTO), SUM(s.VALOR_VENDA) AS TOTAL_VENDAS, SUM((s.VALOR_VENDA / p.PREÇO_KG)) AS Quantidade_Vendida_KG FROM sales s INNER JOIN products p ON s.ID_PRODUTO = p.ID_PRODUTO GROUP BY p.NOME_PRODUTO ORDER BY Quantidade_Vendida_KG DESC LIMIT 1").fetchone()
 
     def best_banana_selling_day(self):
-        result = self.con.execute("SELECT EXTRACT(DAY FROM DATA) AS DAY, s.DATA, SUM(VALOR_VENDA) AS TOTAL_VENDA, COUNT(p.ID_PRODUTO) FROM sales s INNER JOIN products p ON s.ID_PRODUTO = p.ID_PRODUTO  WHERE p.NOME_PRODUTO = 'Banana' GROUP BY DAY, DATA ORDER BY TOTAL_VENDA DESC LIMIT 1").fetchall()
-        print(result)
+        return self.con.execute("SELECT EXTRACT(DAY FROM DATA) AS DAY, s.DATA, SUM(VALOR_VENDA) AS TOTAL_VENDA, COUNT(p.ID_PRODUTO) FROM sales s INNER JOIN products p ON s.ID_PRODUTO = p.ID_PRODUTO  WHERE p.NOME_PRODUTO = 'Banana' GROUP BY DAY, DATA ORDER BY TOTAL_VENDA DESC LIMIT 1").fetchone()
     
 
 if __name__ == '__main__':
@@ -47,7 +38,7 @@ if __name__ == '__main__':
     market_data = MarketData(data_path) 
     market_data.load_sales()
     market_data.load_products()
-    market_data.best_selling_days()
+    market_data.best_selling_day()
     market_data.daily_sales()
     market_data.sales_per_product()
     market_data.best_selling_product_in_kg()
